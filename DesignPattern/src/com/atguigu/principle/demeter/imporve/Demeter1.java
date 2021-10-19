b6668780661232ea7eae5582c78891da07787c72
CloudforJ <bowenlu.cloud@gmail.com>
2021-10-19 15:33:40 +0800

    Demeter Principle

---
+++ DesignPattern/src/com/atguigu/principle/demeter/imporve/Demeter1.java
@@ -0,0 +1,77 @@
+package com.atguigu.principle.demeter.imporve;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Demeter1 {
+    public static void main(String[] args) {
+        SchoolManager schoolManager = new SchoolManager();
+        schoolManager.printAllEmployee(new CollegeManager());
+    }
+}
+
+class Employee {
+    private String id;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+}
+
+class CollegeEmployee {
+    private String id;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+}
+
+class CollegeManager {
+    public List<CollegeEmployee> getAllEmployee() {
+        List<CollegeEmployee> list = new ArrayList<CollegeEmployee>();
+        for (int i = 0; i < 10; i++) {
+            CollegeEmployee emp = new CollegeEmployee();
+            emp.setId("学院员工id= " + i);
+            list.add(emp);
+        }
+        return list;
+    }
+
+    public void printCollegeEmployee() {
+        List<CollegeEmployee> list1 = this.getAllEmployee();
+        System.out.println("----------分校员工----------");
+        for (CollegeEmployee e : list1) {
+            System.out.println(e.getId());
+        }
+    }
+}
+
+class SchoolManager {
+    public List<Employee> getAllEmployee() {
+        List<Employee> list = new ArrayList<Employee>();
+        for (int i = 0; i < 10; i++) {
+            Employee emp = new Employee();
+            emp.setId("学校总部员工id= " + i);
+            list.add(emp);
+        }
+        return list;
+    }
+
+    public void printAllEmployee(CollegeManager sub) {
+        sub.printCollegeEmployee();
+        List<Employee> list2 = this.getAllEmployee();
+        System.out.println("----------学校员工----------");
+        for (Employee e : list2) {
+            System.out.println(e.getId());
+        }
+
+    }
+}
