820d863275967f75a09ff9207f6e2a0e711c5ae2
CloudforJ <bowenlu.cloud@gmail.com>
2021-10-21 15:56:58 +0800

    Update Test.java

--- DesignPattern/src/com/atguigu/singleton/Test.java
+++ DesignPattern/src/com/atguigu/singleton/Test.java
@@ -5,6 +5,7 @@ public class Test {
 
         // IDEA Ctrl+left click to look up Runtime source code
         // 饿汉式单例
+        String hello = "hello world";
         Runtime runtime = Runtime.getRuntime();
         System.out.println(runtime);
     }
